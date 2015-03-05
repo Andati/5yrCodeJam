46ac8bb39137ab556f1a1cc30bfbcfe645d96175
Rodgers Andati <randati@mo-de.com>
2015-03-05 10:02:47 +0300

    final

--- 5yrCodeJam_supp.py
+++ 5yrCodeJam_supp.py
@@ -1,5 +1,5 @@
 
-"""5yrCodeJam.py: Solves the supplementary problem for #5yrCodeJam."""
+"""5yrCodeJam.py: Solves the supplementary problem for #5yrCodeJam. Answer is: 58837602739217760256"""
 
 __author__  = "Rodgers Andati"
 __email__ = "andatirodgers@gmail.com"
@@ -7,28 +7,27 @@ __version__ = "1.0"
 
 import time, math
 
-def sum_digits(num):
-    sum_dig = 0
-    while num > 0:
-        (num, last) = (num / 10, num % 10)
-        sum_dig += last
-    return sum_dig
+def sum_nums(n):
+    sum_nums = 0
+    while n > 0:
+        (n, last_num) = (n / 10, n % 10)
+        sum_nums += last_num
+    return sum_nums
 
 
 if __name__ == '__main__':
 	special_nums = []
-	for i in range(2, 400):
-		temp = i
-		for j in range(2, 100):
-			temp *= i
-			if sum_digits(temp) == i:
-				special_nums.append(temp)
-			if len(special_nums) > 100:
+	for i in range(2, 245):
+		temp_num = i
+		for j in range(2, 25):
+			temp_num *= i
+			if sum_nums(temp_num) == i:
+				special_nums.append(temp_num)
+			if len(special_nums) > 95:
 				break
-		if len(special_nums) > 100:
+		if len(special_nums) > 95:
 			special_nums.sort()
-			#print special_nums
-			print special_nums[78]
+			print str(special_nums[78])[20:]
 			break
 
 
---
+++ README.md
@@ -0,0 +1,8 @@
+# 5yrCodeJam
+
+Solutions to #5yrCodeJam challenge
+
+## About #5yrCodeJam
+
+*   [SCI Website](http://codejam.sci.website/challenge/home/about)
+*   [Ihub Website](http://community.ihub.co.ke/blogs/22497/the-5yrcodejam-challenge-celebrating-5-years-of-writing-code)
